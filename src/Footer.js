2989499489ede17ef2b84a4306f8bc1a875f71b6
RaahilMehta <raahilmehta77@gmail.com>
2024-09-20 20:28:17 +0530

    updated footer links

--- src/Footer.js
+++ src/Footer.js
@@ -7,6 +7,7 @@ function Footer() {
       <a href="https://drive.google.com/file/d/1eO6zXQOL4l4TU4Oakq2gf8ZmE-7qvAKg/view?usp=sharing">Resume</a>
       <a href="https://www.linkedin.com/in/raahil-mehta/">LinkedIn</a>
       <a href="mailto:raahilmehta77@gmail.com">Mail</a>
+      <a href="https://linktr.ee/RaahilMehta">Linktree</a>
     </footer>
   );
 }
